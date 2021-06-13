13f5322eb9a1003834d474f822c8c5dd4427e400
Radosław Sendecki <rsendecki@cirow.pl>
2021-06-13 15:05:20 +0200

    scaffold of app

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeService } from '../services/recipe.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  tytul: string;
+  constructor(private recipeData: RecipeService) {}
 
-  constructor() {}
-
+  ngOnInit(){
+    this.tytul = this.recipeData.title;
+  }
 }
